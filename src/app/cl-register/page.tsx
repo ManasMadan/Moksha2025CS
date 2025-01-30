638ef05feb6f3964f60e4dc4a2c40908200d8c35
singhdsp <146065156+singhdsp@users.noreply.github.com>
2025-01-30 23:52:28 +0400

    Updated CL-Register With New Content

--- src/app/cl-register/page.tsx
+++ src/app/cl-register/page.tsx
@@ -19,70 +19,82 @@ const CLRegister = () => {
       <div className="fixed inset-0 bg-gradient-to-b from-[#1A3BAA] to-transparent mix-blend-overlay" />
       <div className="relative z-10 w-full py-6 sm:py-10 md:py-16 px-4 sm:px-6 md:px-10">
         <div
-          className={`${playfair.className} max-w-7xl mx-auto bg-[#233678CC] rounded-lg sm:rounded-2xl border border-[#00EAE399] px-6 sm:px-8 py-8 sm:py-10 text-white text-base sm:text-xl md:text-2xl lg:text-3xl font-bold space-y-8 sm:space-y-10 md:space-y-14`}
+          className={`${playfair.className} max-w-7xl flex flex-col mx-auto bg-[#233678CC] rounded-lg sm:rounded-2xl border border-[#00EAE399] px-6 sm:px-8 py-8 sm:py-10 text-white text-base sm:text-xl md:text-2xl lg:text-3xl font-bold space-y-8 sm:space-y-10 md:space-y-14`}
         >
           <div className="space-y-3 sm:space-y-4">
             <h1
               className={`${cinzel.className} text-[#20F2ED] tracking-wide font-[549] text-3xl sm:text-4xl md:text-5xl lg:text-6xl`}
             >
-              Contingent Leader Register
+              Contingent Leader Registration
             </h1>
-            <p className="text-base sm:text-xl md:text-2xl lg:text-3xl">
-              The Moksha&apos;s dynamic community, is well-known. Accepting the job of Contingent Leader not only
-              elevates you to the ranks of this group, but it also grants you access to a plethora of benefits:
-            </p>
-            <ul className="list-disc list-inside text-base sm:text-xl md:text-2xl lg:text-3xl ml-2">
-              <li>Certificate of appreciation</li>
-              <li>Gift hampers</li>
-              <li>Official social media shoutout</li>
-              <li>Access to VIP passes</li>
-              <li>Letter of Recommendation and many more surprises</li>
-            </ul>
+            <div className="text-base sm:text-xl md:text-2xl lg:text-3xl space-y-2 sm:space-y-4">
+              <p>
+                Are you ready to lead your squad to ultimate glory and create a legacy that will echo through the ages?
+                Moksha&apos;25 presents a golden opportunity for you to become a Contingent Leader. This is your moment
+                to rise, lead, and leave an everlasting mark.
+              </p>
+              <p>
+                Step into the spotlight, the first step is here‼ Legends aren&apos;t discovered; they are forged through
+                perseverance, brilliance, and courage.
+              </p>
+              <p> Let&apos;s tap into your destiny and lead the way.</p>
+            </div>
           </div>
           <div className="space-y-3 sm:space-y-4">
             <h1
               className={`${cinzel.className} text-[#20F2ED] tracking-wide font-[549] text-3xl sm:text-4xl md:text-5xl lg:text-6xl`}
             >
-              Requirements of CLs
+              Requirements for Contingent Leaders
             </h1>
             <ul className="list-disc list-inside text-base sm:text-xl md:text-2xl lg:text-3xl ml-2">
-              <li>College Student</li>
-              <li>Strong communication skills</li>
-              <li>Efficient decision maker</li>
-              <li>Approachable and team player</li>
               <li>Accountable and reliable</li>
-              <li>Should be active on social media</li>
-              <li>Approachable and team player</li>
-              <li>Should not have disciplinary action against them</li>
+              <li>Approachable and a team player</li>
+              <li>An efficient decision-maker</li>
+              <li>No disciplinary actions against them</li>
+              <li>A regular college student</li>
+              <li>Strong communication skills</li>
+              <li>Active on social media</li>
             </ul>
           </div>
           <div className="space-y-3 sm:space-y-4">
             <h1
               className={`${cinzel.className} text-[#20F2ED] tracking-wide font-[549] text-3xl sm:text-4xl md:text-5xl lg:text-6xl`}
             >
-              Duties as the Contingent Leader
+              Duties of a Contingent Leader
             </h1>
             <ul className="list-disc list-inside text-base sm:text-xl md:text-2xl lg:text-3xl ml-2">
               <li>Act as the bridge between Moksha&apos;25 and your college.</li>
-              <li>
-                Amplify the buzz about Moksha&apos;25 through your active social media presence and word of mouth.
-              </li>
+              <li>Amplify the buzz about Moksha&apos;25 through active social media presence and word-of-mouth.</li>
               <li>Ensure active participation from your college in Moksha&apos;25.</li>
+              <li>Keep your team and participants informed about schedules, rules, and announcements.</li>
             </ul>
           </div>
           <div className="space-y-3 sm:space-y-4">
             <h1
               className={`${cinzel.className} text-[#20F2ED] tracking-wide font-[549] text-3xl sm:text-4xl md:text-5xl lg:text-6xl`}
             >
-              Point Of Contact
+              Perks of Being a Contingent Leader
             </h1>
             <ul className="list-disc list-inside text-base sm:text-xl md:text-2xl lg:text-3xl ml-2">
-              <li>Aditya 7250880898</li>
-              <li>Dipesh 9650538213</li>
-              <li>Lalit Panghal 8178634241</li>
+              <li>Access to VIP areas</li>
+              <li>Certificate of appreciation</li>
+              <li>Gift hampers</li>
+              <li>Social media shoutout & many more surprises!</li>
             </ul>
           </div>
-          <Link href="https://bit.ly/40o1RTs" className="block" target="_blank">
+          <div className="space-y-3 sm:space-y-4">
+            <h1
+              className={`${cinzel.className} text-[#20F2ED] tracking-wide font-[549] text-3xl sm:text-4xl md:text-5xl lg:text-6xl`}
+            >
+              For Any Queries:-
+            </h1>
+            <ul className="list-disc list-inside text-base sm:text-xl md:text-2xl lg:text-3xl ml-2">
+              <li>Aditya:- 7250880898</li>
+              <li>Dipesh:- 9650538213</li>
+              <li>Lalit Panghal:- 8178634241</li>
+            </ul>
+          </div>
+          <Link href="https://forms.gle/kCgoDgecqEbNwB4M6" className="block mx-auto" target="_blank">
             <button className="text-xl md:text-2xl lg:text-3xl bg-[#20F2ED] text-black px-4 py-2 lg:py-4 rounded-md cursor-pointer">
               Register Now
             </button>
